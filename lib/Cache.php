419eb36290a7ed18ac6fe2b0ed200d7686d315c1
BenoitCourville <benoit.courville@outlook.com>
2015-03-27 01:04:58 -0400

    Initial publish

---
+++ lib/Cache.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Benoit
+ * Date: 2015-03-26
+ * Time: 10:36 PM
+ */
+
+function CreateCache($resultSet=null){
+    $cacheID=null;
+
+    return $cacheID;
+}
